3135a9d8f56449c9d2155c295e47b1e3512bd353
Daniel Bälz <info@dbaelz.de>
2025-07-04 12:00:00 +0200

    Add httpClient

--- composeApp/src/commonMain/kotlin/de/dbaelz/siloshare/AppModule.kt
+++ composeApp/src/commonMain/kotlin/de/dbaelz/siloshare/AppModule.kt
@@ -5,11 +5,13 @@ import com.russhwolf.settings.Settings
 import de.dbaelz.siloshare.feature.settings.MultiplatformSettingsRepository
 import de.dbaelz.siloshare.feature.settings.SettingsRepository
 import de.dbaelz.siloshare.feature.settings.SettingsViewModel
+import de.dbaelz.siloshare.network.createHttpClient
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 fun appModule(navHostController: NavHostController) = module {
     single<SettingsRepository> { MultiplatformSettingsRepository(Settings()) }
+    single { createHttpClient(settingsRepository = get()) }
 
     single<ActionDispatcher> { DefaultActionDispatcher(navHostController) }
 
---
+++ composeApp/src/commonMain/kotlin/de/dbaelz/siloshare/network/HttpClient.kt
@@ -0,0 +1,53 @@
+package de.dbaelz.siloshare.network
+
+import de.dbaelz.siloshare.feature.settings.SettingsRepository
+import io.ktor.client.*
+import io.ktor.client.engine.*
+import io.ktor.client.plugins.*
+import io.ktor.client.plugins.auth.*
+import io.ktor.client.plugins.auth.providers.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.json.Json
+
+
+fun createHttpClient(
+    settingsRepository: SettingsRepository,
+    engine: HttpClientEngine? = null
+): HttpClient {
+    val clientConfig: HttpClientConfig<*>.() -> Unit = {
+        install(Auth) {
+            basic {
+                credentials {
+                    BasicAuthCredentials(
+                        username = settingsRepository.getUsername(),
+                        password = settingsRepository.getPassword()
+                    )
+                }
+            }
+        }
+        install(ContentNegotiation) {
+            json(Json {
+                encodeDefaults = true
+                isLenient = true
+                coerceInputValues = true
+                ignoreUnknownKeys = true
+            })
+        }
+
+        defaultRequest {
+            url {
+                host = settingsRepository.getHostAddress()
+                port = settingsRepository.getPort()
+                path("/api/")
+            }
+        }
+    }
+
+    return if (engine == null) {
+        HttpClient(clientConfig)
+    } else {
+        HttpClient(engine, clientConfig)
+    }
+}
